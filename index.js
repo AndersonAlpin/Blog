48667102440e9cae0bb6e51d04a17946e61804e0
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-10 11:07:02 -0300

    .

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database")
+
+// Import Rotas
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+
+// View engine
+app.set('view engine', 'ejs');
+
+// Satic
+app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão realizada com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    })
+
+// Usar as Rotas importadas
+app.use("/", categoriesController);
+app.use("/", articlesController);
+
+// Rota Principal
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("Servidor rodando!")
+});
