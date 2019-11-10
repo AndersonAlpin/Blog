f58c77f965ebf71972b5852bf0cc8447a08b66b6
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-10 12:00:09 -0300

    .

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+
+            function validar(){
+              var email = login.name.value;
+              var senha = login.senha.value;
+          
+              if(nome == ""){
+                  alert("digite seu email")
+                  loginEmail.name.focus() 
+                  return false;
+              }
+          
+              if(senha == ""){
+                  alert("digite sua senha");
+                  loginEmail.senha.focus();
+              }
+          }
+    
