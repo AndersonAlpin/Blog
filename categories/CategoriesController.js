5c8dce508db7d5323d0049c2f94c28b98304e984
Anderson Alpin <48166236+AndersonAlpin@users.noreply.github.com>
2019-11-14 20:38:11 -0300

    Deletando categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Categorie = require("./Category");
+const Category = require("./Category");
 const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
@@ -10,7 +10,7 @@ router.get("/admin/categories/new", (req, res) => {
 router.post("/categories/save", (req, res) => {
     var title = req.body.title;
     if(title != undefined){
-        Categorie.create({
+        Category.create({
             title: title,
             slug: slugify(title)
         }).then(() => {
@@ -22,10 +22,31 @@ router.post("/categories/save", (req, res) => {
 });
 
 router.get("/admin/categories", (req, res) => {
-    Categorie.findAll({ raw: true}).then(categories => { 
+    Category.findAll({ raw: true}).then(categories => { 
         res.render("admin/categories/index", {categories: categories});
     });
 });
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){ //SE FOR DIFERENTE DE NULO
+        if(!isNaN(id)){ //SE FOR UM NÚMERO
+
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+
+        }else { //SE NÃO FOR UM NÚMERO
+            res.redirect("/admin/categories");
+        }
+    }else { //SE FOR NULO
+        res.redirect("/admin/categories");
+    }
+})
 
 
 module.exports = router;
